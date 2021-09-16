2e38026771253947527d30f610701f3bcf28d23b
iamsmurph <hikaru.murphy@gmail.com>
2021-09-16 16:27:07 -0400

    generated 4/6 datasets in 2 different ways

--- process.py
+++ process.py
@@ -2,12 +2,16 @@ from organoid import preprocessing
 import numpy as np
 from collections import Counter
 
-# loading centroid arrays
-initPattern = np.load("datasets/random3/initRandom3.npy")
-finalPattern = np.load("datasets/random3/coordsRewardRandom3.npy")
+# experiment id
+expNum = 7
 
-# experiment name
-dir = "random3"
+#data dir
+dir = "random7_design"
+
+# loading centroid arrays
+initPattern = np.load("datasets/" + dir + "/initRandom" + str(expNum) + ".npy")
+finalPattern = np.load("datasets/" + dir + "/coordsRewardRandom" + str(expNum) + ".npy")
+
 # instantiate preprocessing class with save dir path
 p = preprocessing.Alignment(dir)
 
