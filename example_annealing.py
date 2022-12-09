22e07b5ec7e650a8765e87f5f650b4cb54e8bc20
Sean Murphy <seanmurphy@college.harvard.edu>
2022-12-09 14:25:08 -0500

    cleaning

--- example_annealing.py
+++ example_annealing.py
@@ -1,16 +1,13 @@
 from pipeline.symbreakanneal import SymBreakAnneal
 
 pipeline = SymBreakAnneal(
-        data_path = 'data/example_coords.csv',
         save_path = "results/",
-        save_plots = True
+        save_plots = True,
+        n_total_orgs = 6
     )
 
-    # image generation
-im, centroids = pipeline.sample_pattern(save_samples = True)
+# sample coordinates
+centroids = pipeline.sample_pattern()
 
 # sim annealing
-#new_im, new_centroids, log = pipeline.sim_anneal(im, centroids.tolist())
-
-# save larger files manually in run
-#pickle.dump(log, open(os.path.join("results/","anneal_log.txt"), "wb"))
+optim_centroids = pipeline.sim_anneal(centroids)
